78a91d292d8b89be347fa1c6a77f230f6ae7a048
Miika Kanerva <miika.kanerva@aalto.fi>
2018-10-30 14:18:53 +0200

    TR CSV fetches data from people who got accepted grant class

--- app/client/views/admin/users/adminUsersCtrl.js
+++ app/client/views/admin/users/adminUsersCtrl.js
@@ -969,7 +969,7 @@ angular.module('reg')
         .getAll()
         .success(function(data){
           data = data.filter(function(user){
-            return user.status.reimbursementApplied;
+            return user.profile.AcceptedreimbursementClass && user.profile.AcceptedreimbursementClass !== "None";
           })
           var output = "";
           var titles = generateTRSections(data[0]);
