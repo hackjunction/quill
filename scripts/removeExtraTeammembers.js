40c773ccb7bc07ad4b04defa043bbccbc6629bc6
Henrik Aarnio <hjaarnio@gmail.com>
2018-11-24 18:16:55 +0200

    fix teammember update script

--- scripts/removeExtraTeammembers.js
+++ scripts/removeExtraTeammembers.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 var mongoose        = require('mongoose');
 var database        = process.env.DATABASE || { url: "mongodb://localhost:27017"};
 mongoose.connect(database.url);
